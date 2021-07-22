2449b28a983af0834b5b27a32bdea72b5ce484e8
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-21 21:03:25 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function writeCards(arr1, evnt){
+  let arr2 = []
+  for (let i=0; i < arr1.length; i++){
+    arr2.push(`Thank you, ${arr1[i]}, for the wonderful ${evnt} gift!`)
+  }
+  return arr2
+}
+writeCards(['Lisa', 'Kaitlin', 'Jan'], 'birthday') 
+
+// function countDown() {
+//   while (countdown > 10) {
+//     countdown--
+//   }
+// }
+
 function countDown(num) {
   let i = num
   while (i >= 0) {
@@ -8,3 +23,5 @@ function countDown(num) {
 }
 
 countDown(10)
+
+
